89eeb065530ce408d4993d3ec192c66acf15d9d4
Ken Sin <ksin@movableink.com>
2015-10-28 08:57:35 -0700

    Use MI url directly instead of making post request

--- awyiss.rb
+++ awyiss.rb
@@ -2,6 +2,7 @@ require "slack-ruby-client"
 require "net/http"
 require "uri"
 require "json"
+require "cgi"
 
 puts "WARNING!!! awyisser tweets all your yisses to @awyisser, so maybe don't put anything you don't want tweeted?"
 
@@ -26,11 +27,30 @@ module AwYisser
   def get_image(phrase)
     params = {"phrase" => phrase}
     params["sfw"] = true if sfw # required to only add sfw key conditionally because {"sfw" => false} still registers as true
-    response = httpPostRequest(URL, params)
+    #post_to_awyisser(params)
+    direct_image_link(params)
+  end
+
+  def direct_image_link(params)
+    "#{URL}?#{parameterize(params)}"
+  end
+
+  def parameterize(params)
+    encoded_params = params.collect do |k,v|
+      if v.is_a? String
+        v = CGI::escape(v)
+      end
+      "#{k}=#{v}"
+    end
+    encoded_params.join('&')
+  end
+
+  def post_to_awyisser(params)
+    response = http_post_request(URL, params)
     JSON.parse(response.body)["link"]
   end
 
-  def httpPostRequest(url, data)
+  def http_post_request(url, data)
     uri = URI.parse(url)
     http = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Post.new(uri.request_uri)
